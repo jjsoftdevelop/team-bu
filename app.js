f2833a91f93616e21b1cf2c1e9345926358c5dd1
jason hong <a10010134@gmail.com>
2022-04-15 17:00:07 +0800

    測試 session

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         // secure: process.env.NODE_ENV !== 'dev',
+        sameSite: 'none',
         maxAge: 1000 * 60 * 99999,
         httpOnly: true,
     }
