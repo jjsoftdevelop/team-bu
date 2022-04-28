9f7fcac29044092e499791f9886a7e5f057388df
yixianghong <a10010134@gmail.com>
2022-04-28 16:46:47 +0800

    csrf

--- src/server/middleware/csrf.js
+++ src/server/middleware/csrf.js
@@ -2,11 +2,7 @@ const csurf = require('csurf')
 const csrf = csurf({ cookie: true })
 
 const checkCsrf = (req, res, next) => {
-    const ip =
-        req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        (req.connection.socket ? req.connection.socket.remoteAddress : null)
+    const ip = req.headers['x-real-ip'] || req.connection.remoteAddress || null
     if (
         req.hostname.includes('127.0.0.1') ||
         ip.includes('127.0.0.1')
