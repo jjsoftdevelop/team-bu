2219670458c5c16eec3ead01b0691aec72d8f0b3
jason hong <a10010134@gmail.com>
2022-05-15 19:04:14 +0800

    update ip csrf token include

--- src/server/middleware/csrf.js
+++ src/server/middleware/csrf.js
@@ -5,7 +5,7 @@ const checkCsrf = (req, res, next) => {
     const ip = req.headers['x-real-ip'] || req.connection.remoteAddress || null
     if (
         req.hostname.includes('127.0.0.1') ||
-        ip.includes('127.0.0.1') || req.hostname.includes('123.194.118.42') || ip.includes('123.194.118.42')
+        ip.includes('127.0.0.1') || req.hostname.includes('34.81.184.52') || ip.includes('34.81.184.52')
     ) {
         next()
     } else {
